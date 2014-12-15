8a88e4cec16101faee5fb78fb25d34f35f1ec89a
Travis Swicegood <development@domain51.com>
2014-12-15 14:22:25 -0800

    Make sure to add in a default handler

--- application/simple/views.py
+++ application/simple/views.py
@@ -9,4 +9,7 @@ blueprint = Blueprint('application', __name__, template_folder='templates')
 @blueprint.route('/', defaults={'page': 'index'})
 @blueprint.route('/<page>/')
 def index(page):
+    if page == "favicon.ico":
+        return ""
+
     return render_template('{}.html'.format(page))

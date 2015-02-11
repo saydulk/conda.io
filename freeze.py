cf3e303535bd9aa39b2bfe2ab9c4148599c78458
Travis Swicegood <development@domain51.com>
2015-02-11 10:02:39 -0800

    Nix custom -- we'll just always serve at root

--- freeze.py
+++ freeze.py
@@ -1,5 +1,3 @@
-import re
-
 from flask_frozen import Freezer
 
 from application import create_app
@@ -10,43 +8,13 @@ app.config.update({
     'FREEZER_RELATIVE_URLS': True,
 })
 
-
-class MyFreezer(Freezer):
-    """
-    Custom Freezer class to handle patching URLs
-
-    continuum_flask's Flask object adds some helpers that are missed by the
-    relative url settings of Frozen Flask.  This is a rudimentary way of making
-    some of those URLs relative.
-
-    All pages that end with .html are parsed and any pages
-    """
-    def _build_one(self, url):
-        filename = super(MyFreezer, self)._build_one(url)
-
-        # Skip any non HTML pages
-        if not filename.endswith(".html"):
-            return filename
-
-        with open(filename, "r") as f:
-            contents = f.read()
-        contents = re.sub(r"(href|src)=([\"'])/",
-                          r"\1=\2./",
-                          contents)
-        # contents = contents.replace('href="/', 'href="./')
-        # contents = contents.replace('src="/', 'src="./')
-        #
-        with open(filename, "w") as f:
-            f.write(contents)
-        return filename
-
-
-freezer = MyFreezer(app)
+freezer = Freezer(app)
 
 
 @freezer.register_generator
 def urls():
     yield 'application.index', {'page': 'index'}
+    yield 'application.index', {'page': 'style-guide'}
 
 
 if __name__ == '__main__':
